8798ca4aba133f101ca5fe65780aa12e2429db48
harshavardhanrajan21 <harshavardhan.rajan@gmail.com>
2021-09-03 02:07:07 +1000

    Modified Dockercompose
    Modified Docker Compose to build images from relative Docker files.

--- client-service/src/test/java/au/com/company/clientservice/ClientServiceApplicationTests.java
+++ client-service/src/test/java/au/com/company/clientservice/ClientServiceApplicationTests.java
@@ -1,10 +1,13 @@
 package au.com.company.clientservice;
 
+
+
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class ClientServiceApplicationTests {
 
-
+    public void contextLoads() {
+    }
 }
